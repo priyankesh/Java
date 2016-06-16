27b37c5591e100c35b9ddc6fe8b2bc01d2207bbb
Nigam <priyankesh.nigam@accenture.com>
2016-06-16 18:27:52 +0530

    Initil commit for Fibonacci

---
+++ Fibonacci/Fibonacci.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Fibonacci {
+    private static List<Integer> memoList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Fibonacci fib = new Fibonacci();
+        memoList.add(0);
+        for (int a = 0; a <= 10; a++)
+            System.out.println(a + " Fibonacci: " + fib.fibo(a));
+    }
+
+    private int fibo(int n) {
+        int f;
+        if (n < memoList.size())
+            return memoList.get(n);
+        if (n <= 2) {
+            f = 1;
+        } else {
+            f = fibo(n - 1) + fibo(n - 2);
+        }
+        memoList.add(f);
+        return f;
+    }
+}
