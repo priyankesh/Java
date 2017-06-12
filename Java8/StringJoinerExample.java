dbce338cb028606d47f876af7f4b86c339adf030
Priyankesh Nigam <Priyankesh Nigam>
2017-06-12 21:11:48 +0530

    Adding Java 8 examples

---
+++ Java8/StringJoinerExample.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
+
+public class StringJoinerExample {
+
+	public static void main(String[] args) {
+		StringJoiner sj = new StringJoiner(":", "{", "}"); // delimiter, prefix, suffix
+		sj.add("Ram").add("Shyam").add("Govind");
+		System.out.println(sj.toString());
+
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+		String commaSeparatedNumbers = numbers.stream().map(i -> i.toString()).collect(Collectors.joining(", "));
+		System.out.println(commaSeparatedNumbers);
+	}
+}
