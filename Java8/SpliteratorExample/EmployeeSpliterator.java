3fad904305cf5e2262022c7e4ff40eeb160d5af2
Priyankesh Nigam <priyankesh@gmail.com>
2017-09-07 09:39:44 +0530

    Spliterator Example

---
+++ Java8/SpliteratorExample/EmployeeSpliterator.java
@@ -0,0 +1,45 @@
+package SpliteratorExample;
+
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class EmployeeSpliterator implements Spliterator<Employee> {
+
+  private Spliterator<String> lineSpliterator;
+  private String name;
+  private int age;
+  private String department;
+
+  public EmployeeSpliterator(Spliterator<String> lineSpliterator) {
+    this.lineSpliterator = lineSpliterator;
+  }
+
+  @Override
+  public boolean tryAdvance(Consumer<? super Employee> action) {
+    if (this.lineSpliterator.tryAdvance(line -> this.name = line)
+        && this.lineSpliterator.tryAdvance(line -> this.age = Integer.parseInt(line))
+        && this.lineSpliterator.tryAdvance(line -> this.department = line)) {
+
+      Employee e = new Employee(name, age, department);
+      action.accept(e);
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public Spliterator<Employee> trySplit() {
+    return null; // It is used for parallel processing.
+  }
+
+  @Override
+  public long estimateSize() {
+    return lineSpliterator.estimateSize() / 3; // We have groups of three in EmployeeDetails.txt file.
+  }
+
+  @Override
+  public int characteristics() {
+    return lineSpliterator.characteristics();
+  }
+
+}
---
+++ Java8/SpliteratorExample/ReadFileUsingSpliterator.java
@@ -0,0 +1,28 @@
+package SpliteratorExample;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Spliterator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class ReadFileUsingSpliterator {
+
+  public static void main(String[] args) {
+    Path path = Paths.get("D:/MyWork/EmployeeDetails.txt"); // Use file given here and place it at some location and use
+                                                            // that path here.
+    try (Stream<String> lines = Files.lines(path)) {
+
+      Spliterator<String> lineSpliterator = lines.spliterator();
+      Spliterator<Employee> employeeSpliterator = new EmployeeSpliterator(lineSpliterator);
+
+      Stream<Employee> employee = StreamSupport.stream(employeeSpliterator, false);
+      employee.forEach(System.out::println);
+
+    } catch (IOException ioe) {
+      ioe.printStackTrace();
+    }
+  }
+}
