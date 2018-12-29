c0e812d9117e7f53b56bbb5c09f7d50dadaec717
= <=>
2018-12-29 18:03:32 +0530

    commit

--- Java8/Streams/Streams1.java
+++ Java8/Streams/Streams1.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 public class Streams1 {
     public static void main(String[] args) {
         String vegetables = Stream.of("Carrot", "Cauliflower", "Beetroot", "Cabbage", "Garlic")
+                .sorted()
                 .collect(Collectors.joining(", "));
         System.out.println(vegetables);
     }
